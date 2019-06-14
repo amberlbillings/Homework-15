26bddd1d1c67146726ad186aa906b189a4a7307e
Amber Billings <hello@amberbillings.com>
2019-06-14 00:12:24 -0500

    code files updated

--- static/js/bonus.js
+++ static/js/bonus.js
@@ -1,1 +1,62 @@
+function buildGauge(wFreq) {
+ // Enter a speed between 0 and 180
+    var level = parseFloat(wFreq) * 20;
 
+    // Trig to calc meter point
+    var degrees = 180 - level,
+        radius = .5;
+    var radians = degrees * Math.PI / 180;
+    var x = radius * Math.cos(radians);
+    var y = radius * Math.sin(radians);
+
+    // Path: may have to change to create a better triangle
+    var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+        pathX = String(x),
+        space = ' ',
+        pathY = String(y),
+        pathEnd = ' Z';
+    var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+    var data = [{ type: 'scatter',
+    x: [0], y:[0],
+        marker: {size: 28, color:'850000'},
+        showlegend: false,
+        name: 'Wash Frequency',
+        text: level/20,
+        hoverinfo: 'text+name'},
+    { values: [1/9, 1/9, 1/9, 1/9, 1/9, 1/9, 1/9, 1/9, 1/9, 1],
+    rotation: 90,
+    text: ['8-9', '7-8', '6-7', '5-6',
+                '4-5', '3-4', '2-3', '1-2', '0-1', ''],
+    textinfo: 'text',
+    textposition:'inside',
+    marker: {colors:['#45A46D', '#5CAA70', '#70B173',
+                    '#81B876', '#92BF7A', '#A3C77D', 
+                    '#B4D080', '#C5D983', '#D7E487', 'white']},
+    labels: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '1-2', '0-1', ''],
+    hoverinfo: 'label',
+    hole: .5,
+    type: 'pie',
+    showlegend: false
+    }];
+
+    var layout = {
+    shapes:[{
+        type: 'path',
+        path: path,
+        fillcolor: '850000',
+        line: {
+            color: '850000'
+        }
+        }],
+    title: '<b>Belly Button Washing Frequency</b> <br> Scrubs per Week',
+
+    xaxis: {zeroline:false, showticklabels:false,
+                showgrid: false, range: [-1, 1]},
+    yaxis: {zeroline:false, showticklabels:false,
+                showgrid: false, range: [-1, 1]}
+    };
+
+    Plotly.newPlot('gauge', data, layout);
+
+}
